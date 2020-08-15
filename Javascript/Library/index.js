5cdc45b4df5808120e16a03169eedc733179163a
Michal Hajdo <smephey@gmail.com>
2020-08-15 20:12:05 +0100

    Testing potential solutions

--- Javascript/Library/index.js
+++ Javascript/Library/index.js
@@ -7,16 +7,32 @@ function Book(title, author, length, read) {
     this.read = read;
 }
 
-Book.prototype.info = function () {
-    return `${this.title} by ${this.author}, ${this.length} pages, ${
-        this.read ? "read already" : "not read yet"
-    }`;
-};
+// Book.prototype.info = function () {
+//     return `${this.title} by ${this.author}, ${this.length} pages, ${
+//         this.read ? "read already" : "not read yet"
+//     }`;
+// };
 
-function addBookToLibrary(book) {
-    library.push(book)
+function addBookToLibrary(title, author, length, read) {
+    const book = new Book(title, author, length, read);
+    library.push(book);
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R Tolkien', 405, true);
-addBookToLibrary(hobbit);
-console.log('current library: ', library)
+function render() {
+    const container = document.getElementById('container');
+
+    library.forEach((book) => {
+        const bookTitle = book.title;
+        let titleContainer = document.createElement('h1');
+        titleContainer.innerText = bookTitle
+        container.appendChild(titleContainer);
+    });
+
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 405, true);
+addBookToLibrary('Lord of the Rings: Fellowship of the Ring', 'J.R.R Tolkien', 600, true);
+addBookToLibrary('Lord of the Rings: Two Towers', 'J.R.R Tolkien', 567, false);
+addBookToLibrary('Lord of the Rings: Return of the King', 'J.R.R Tolkien', 645, false);
+
+render();
