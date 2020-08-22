ef2178036c40dac6b2ceb672c093562aed13f71c
Michal Hajdo <smephey@gmail.com>
2020-08-22 16:27:02 +0100

    Setting up some modules and factories

--- Javascript/TicTacToe/app.js
+++ Javascript/TicTacToe/app.js
@@ -1,1 +1,15 @@
-console.log('tictactoe setup');
+const gameBoard = (() => {
+    let gameboardState = [];
+    return {gameboardState}
+})();
+
+const gameController = (() => {
+})();
+
+const player = (name) => {
+    const getName = () => name;
+    return {getName};
+};
+
+const player1 = player('Test Subject 1');
+const player2 = player('Test Subject 2');
