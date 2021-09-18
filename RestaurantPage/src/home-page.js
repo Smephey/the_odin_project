25c8fdbd30bd19b0addcabec52e504a4ac7e2465
Michal Hajdo <smephey@gmail.com>
2021-09-18 17:52:45 +0100

    Converting to dynamically generating index.html

--- RestaurantPage/src/home-page.js
+++ RestaurantPage/src/home-page.js
@@ -1,7 +1,9 @@
 const loadHomePage = () => {
-  const contentDiv = document.querySelector('#content');
+  const contentDiv = document.createElement('div');
   const title = document.createElement('h1');
   const dashboardCopy = document.createElement('h3');
+
+  contentDiv.id = 'content';
 
   title.innerHTML = 'Cake and Bake';
   title.classList.add('title');
@@ -10,6 +12,7 @@ const loadHomePage = () => {
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ipsam mollitia quam cum dolore facilis unde sit sunt distinctio quo. Eaque optio nisi nihil quisquam? Nostrum, ipsum officia. Nulla, perferendis?';
   dashboardCopy.classList.add('dashboard-copy');
 
+  document.body.appendChild(contentDiv);
   contentDiv.appendChild(title);
   contentDiv.appendChild(dashboardCopy);
 };
--- RestaurantPage/webpack.config.js
+++ RestaurantPage/webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
     homePage: './src/home-page.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
