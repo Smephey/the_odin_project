d015232a4cc2547efcff69fecd40950c3918637e
Michal Hajdo <smephey@gmail.com>
2021-10-18 21:12:23 +0100

    Wrapping up the basic implementation - unsafe and not mobile friendly but enough practice.

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -1,2 +1,36 @@
+const API_KEY = '4cc3dcdd5c4251d4d5aaf16a59d39ac0';
 
+const cityForDisplayContainer = document.querySelector('#current-city');
+const temperatureForDisplayContainer = document.querySelector(
+  '#current-temperature'
+);
+const searchInput = document.querySelector('#search-input');
 
+initialise();
+
+function initialise() {
+  searchInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+      getWeatherForCity(e.target.value.toString());
+    }
+  });
+
+  const response = getWeatherForCity('Lisbon');
+  response;
+}
+
+async function getWeatherForCity(city) {
+  const weatherResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+
+  await weatherResponse
+    .json()
+    .then((weatherData) => {
+      cityForDisplayContainer.innerHTML = weatherData.name;
+      temperatureForDisplayContainer.innerHTML = weatherData.main.temp;
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
+}
